1dc00f59bb109925dadf9deec408e57a0c7631b1
wally1997 <walterbolivar97@gmail.com>
2022-10-06 09:22:36 -0500

    unión archivos

--- App.js
+++ App.js
@@ -2,6 +2,14 @@ const express = require ("express")
 
 const app = express()
 app.get("/", function (req, res){
-    res.send("Hola mundillo!!")
+    res.send("Hello monillo")
 })
-app.listen(3000)
+app.get("/index", function (req, res){
+    res.sendFile(__dirname + "/index.html")
+})
+app.get("/error", function (req, res){
+    res.sendFile(__dirname + "/erorrrrrrrr.PNG")
+})
+app.listen(3001)
+console.log("server port 3001")
+
